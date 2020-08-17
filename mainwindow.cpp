0affc2d060876b63062175b4612211a0694baea3
djord <djord@DESKTOP-EA378QQ>
2020-08-18 01:32:15 +0200

    Adding new tabs and functionalities to the editor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPrintDialog>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,3 +18,89 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_actionNew_triggered()
+{
+    //brisanje stringa
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    //otvaranje fajla
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot open file : " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file); //interface za text
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    //cuvanje fajla
+    //otvaranje prozora za fajl
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Cannot save file : " + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionPrint_triggered()
+{
+    //stampanje
+    QPrinter printer;
+    printer.setPrinterName("Printer name");
+    QPrintDialog pDialog(&printer, this); //prozor za biranje printera
+    if(pDialog.exec() == QDialog::Rejected){
+        QMessageBox::warning(this, "Warning", "Cannot access printer");
+        return;
+    }
+    ui->textEdit->print(&printer);
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    //izlazak iz programa
+    QApplication::quit();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
